c0c4a84d9e85aee7177426f310a84dafb52186a1
Hasan Misbah <hasanmisbah01@gmail.com>
2016-05-30 18:33:15 +0600

    added social button home page and all post head

--- category-11.php
+++ category-11.php
@@ -34,8 +34,17 @@
 						<h5 class="author-info pull-left">বিভাগঃ <?php the_category(', ') ?></h5>
 					    <h5 class="author-info pull-right"><?php the_meta(); ?></h5>
 					    <div style="clear:both; margin-top:10px !important;"></div>
-					    <h5 class="fb-like pull-left"><div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div></h5>
-                        <div style="clear:both"></div>
+					    <div class="social pull-left">
+                	<span class="twitter">
+                		<a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>">Tweet</a>
+                	</span>
+                	<span class="google">
+                		<g:plusone size="large" href="<?php the_permalink(); ?>"></g:plusone>
+                	</span>
+                	<span class="Facebook">
+                		<div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+                	</span>
+                </div>
                         <div style="clear:both"></div>
                         
                         <div class="entry">
--- index.php
+++ index.php
@@ -29,8 +29,18 @@
 									<h5 class="author-info pull-left">বিভাগঃ <?php the_category(', ') ?></h5>
 					                <h5 class="author-info pull-right"><?php the_meta(); ?></h5>
 					                <div style="clear:both"></div>
-					                <h5 class="fb-like pull-left"><div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div></h5>
-                                     <div style="clear:both"></div>
+					                <div class="social">
+                                    	<span class="twitter">
+                                    		<a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>">Tweet</a>
+                                    	</span>
+                                    	<span class="google">
+                                    		<g:plusone size="large" href="<?php the_permalink(); ?>"></g:plusone>
+                                    	</span>
+                                    	<span class="Facebook">
+                                    		<div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+                                	</span>
+                                </div>
+					                
                                      <div style="clear:both"></div>
                                      <?php the_excerpt(); ?>
 								</li>
@@ -48,9 +58,19 @@
 									<h5 class="author-info pull-left">বিভাগঃ <?php the_category(', ') ?></h5>
 					                <h5 class="author-info pull-right"><?php the_meta(); ?></h5>
 					                <div style="clear:both"></div>
-					                <h5 class="fb-like pull-left"><div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div></h5>
-                                     <div style="clear:both"></div>
-                                     <div style="clear:both"></div>
+					                <div class="social">
+                                    	<span class="twitter">
+                                    		<a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>">Tweet</a>
+                                    	</span>
+                                    	<span class="google">
+                                    		<g:plusone size="large" href="<?php the_permalink(); ?>"></g:plusone>
+                                    	</span>
+                                    	<span class="Facebook">
+                                    		<div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+                                	</span>
+                                </div>
+					                
+					                <div style="clear:both"></div>
 									<?php the_excerpt(); ?>
 								</li>
 								<?php endwhile; ?>
@@ -68,8 +88,17 @@
 									<h5 class="author-info pull-left">বিভাগঃ <?php the_category(', ') ?></h5>
 					                <h5 class="author-info pull-right"><?php the_meta(); ?></h5>
 					                <div style="clear:both"></div>
-					                <h5 class="fb-like pull-left"><div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div></h5>
-                                     <div style="clear:both"></div>
+					                <div class="social">
+                                    	<span class="twitter">
+                                    		<a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>">Tweet</a>
+                                    	</span>
+                                    	<span class="google">
+                                    		<g:plusone size="large" href="<?php the_permalink(); ?>"></g:plusone>
+                                    	</span>
+                                    	<span class="Facebook">
+                                    		<div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+                                	</span>
+                                </div>
                                      <div style="clear:both"></div>
 									<?php the_excerpt(); ?>
 								</li>
